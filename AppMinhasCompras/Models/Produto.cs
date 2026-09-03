4868c226b10cfe9a257ec9e4329a26c33c85bd91
arthurvivo <arthurvivoapenas@gmail.com>
2026-09-03 17:56:24 -0300

    Ajustes

--- AppMinhasCompras/Models/Produto.cs
+++ AppMinhasCompras/Models/Produto.cs
@@ -5,11 +5,25 @@ using SQLite;
 
 namespace AppMinhasCompras.Models
 {
+
     public class Produto
     {
+        string descricao;
+
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { 
+            get => descricao;
+            set 
+            { 
+                if(value == null)
+                {
+                    throw new Exception("Descrição não pode ser vazia");
+                }
+                
+                descricao = value;
+            } }
         public double Preco { get; set; }
         public double Quantidade { get; set; }
 
--- AppMinhasCompras/Views/EditarProduto.xaml.cs
+++ AppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,37 +1,9 @@
-using AppMinhasCompras.Models;
-
-namespace AppMinhasCompras.Views;
-
-public partial class EditarProduto : ContentPage
-{
-	public EditarProduto()
-	{
-		InitializeComponent();
-	}
-
-    private async void ToolbarItem_Clicked(object sender, EventArgs e)
-    {
-        try
-        {
-            Produto produto_anexado = BindingContext as Produto
-
-            Produto p = new Produto
-            {
-                Id = produto_anexado.Id, 
-                Descricao = txt_descricao.Text,
-                Preco = Convert.ToDouble(txt_preço.Text),
-                Quantidade = Convert.ToDouble(txt_quantidade.Text)
-
-            };
-
-            await App.Db.UpdateProduto(p);
-            await DisplayAlertAsync("Foi", "Registro Atualizado", "Ok");
-            Navigation.PopAsync();
-
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlertAsync("Naes", ex.Message, "Okay");
-        }
-    }
-}
+<ContentPage x:Class="AppMinhasCompras.Views.EditarProduto"
+             xmlns="http://xamarin.com/schemas/2014/forms"
+             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml">
+  <StackLayout>
+    <Entry x:Name="txt_descricao"   Text="{Binding Descricao}"     Placeholder="Descrição" />
+    <Entry x:Name="txt_preco"       Text="{Binding Preco}"         Keyboard="Numeric" Placeholder="Preço" />
+    <Entry x:Name="txt_quantidade"  Text="{Binding Quantidade}"    Keyboard="Numeric" Placeholder="Quantidade" />
+  </StackLayout>
+</ContentPage>
